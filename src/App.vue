a627c3bf40aae26df05a5dbc5d3d4c28e130b8f7
Timm <timmhinsch@gmail.com>
2025-03-25 11:25:28 +0100

    deploy project

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <h1>Heloo World</h1>
       </nav>
     </div>
   </header>
